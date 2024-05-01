ea3ee4386e3b938002695a5e514a64d5486941dc
“emilesherrott” <emilesherrott@gmail.com>
2024-05-01 18:16:48 +0100

    generic commit

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -45,9 +45,9 @@ module "lfacademy-cluster" {
   eks_managed_node_groups = {
     default = {
       instance_types = ["t2.micro"]
-      min_size       = 1
-      max_size       = 10
-      desired_size   = 1
+      min_size       = 3
+      max_size       = 5
+      desired_size   = 3
     }
   }
 }
