924390a67a1aea4c12cdd94cf257b1a19c8fdb40
“emilesherrott” <emilesherrott@gmail.com>
2024-05-01 15:35:07 +0100

    generic commit

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -26,9 +26,9 @@ data "aws_subnets" "subnets" {
 }
 
 provider "kubernetes" {
-  # host                   = data.aws_eks_cluster.cluster.endpoint
-  # cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  # token                  = data.aws_eks_cluster_auth.cluster.token
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 
@@ -54,31 +54,31 @@ module "lfacademy-cluster" {
 
 
 
-# data "aws_eks_cluster" "cluster" {
-#   name = module.lfacademy-cluster.cluster_id
-# }
+data "aws_eks_cluster" "cluster" {
+  name = module.lfacademy-cluster.cluster_id
+}
 
-# data "aws_eks_cluster_auth" "cluster" {
-#   name = module.lfacademy-cluster.cluster_id
-# }
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.lfacademy-cluster.cluster_id
+}
 
 
 
-# resource "kubernetes_cluster_role_binding" "example" {
-#   metadata {
-#     name = "fabric8-rbac"
-#   }
-#   role_ref {
-#     api_group = "rbac.authorization.k8s.io"
-#     kind      = "ClusterRole"
-#     name      = "cluster-admin"
-#   }
-#   subject {
-#     kind      = "ServiceAccount"
-#     name      = "default"
-#     namespace = "default"
-#   }
-# }
+resource "kubernetes_cluster_role_binding" "example" {
+  metadata {
+    name = "fabric8-rbac"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "default"
+    namespace = "default"
+  }
+}
 
 
 provider "aws" {
