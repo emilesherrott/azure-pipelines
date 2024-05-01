2e0888fff3fc138ca44584a306c99c3fdf9f405e
“emilesherrott” <emilesherrott@gmail.com>
2024-05-01 16:09:11 +0100

    generic commit

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -55,11 +55,11 @@ module "lfacademy-cluster" {
 
 
 data "aws_eks_cluster" "cluster" {
-  name = module.lfacademy-cluster.arn
+  name = module.lfacademy-cluster.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.lfacademy-cluster.arn
+  name = module.lfacademy-cluster.cluster_name
 }
 
 
